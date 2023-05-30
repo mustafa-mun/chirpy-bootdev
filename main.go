5d127f813097a28204ce007a87236ee642af80ca
mustafa <mustafamcm9@gmail.com>
2023-05-30 17:22:45 +0300

    stateful handlers assignment

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
 	port := "8080"
+	filepathRoot := "."
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	apiCfg := apiConfig{fileserverHits: 0}
+	mux.Handle("/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot))))
 	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: corsMux,
@@ -35,4 +43,16 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits += 1
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits: " + strconv.Itoa(cfg.fileserverHits)))
 }
