921c8f5b1d1b72926e74e4864c6d76bae225163c
mustafa <mustafamcm9@gmail.com>
2023-06-01 19:47:29 +0300

    base structure of database package

---
+++ internal/database/database.go
@@ -0,0 +1,59 @@
+package database
+
+import (
+	"sync"
+)
+
+/*
+ Useful standart library functions to know about;
+ -os.ReadFile
+ -os.ErrNotExist
+ -os.WriteFile
+ -sort.Slice
+*/
+
+type DB struct {
+	path string
+	mux  *sync.RWMutex
+}
+
+type DBStructure struct {
+	Chirps map[int]Chirp `json:"chirps"`
+}
+
+// NewDB creates a new database connection
+// and creates the database file if it doesn't exist
+func NewDB(path string) (*DB, error) {
+	
+}
+
+type Chirp struct {
+	ID   int    `json:"id"`
+	Body string `json:"body"`
+}
+
+// CreateChirp creates a new chirp and saves it to disk
+func (db *DB) CreateChirp(body string) (Chirp, error) {
+
+}
+
+
+// GetChirps returns all chirps in the database
+func (db *DB) GetChirps() ([]Chirp, error) {
+
+}
+
+// ensureDB creates a new database file if it doesn't exist
+func (db *DB) ensureDB() error {
+
+}
+
+// loadDB reads the database file into memory
+func (db *DB) loadDB() (DBStructure, error) {
+
+}
+
+// writeDB writes the database file to disk
+func (db *DB) writeDB(dbStructure DBStructure) error  {
+
+}
